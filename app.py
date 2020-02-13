85d4fd0eac8514afc8f412f541fe7939eef9698e
maxnezdyur <maxnezdyur@gmail.com>
2020-02-12 20:27:56 -0500

    adding new graph

--- app.py
+++ app.py
@@ -1,35 +1,60 @@
-from vega_datasets import data
 import streamlit as st
-import altair as alt
+import numpy as np
+from bokeh.plotting import figure, output_file, show
+
 
 def main():
-    df = load_data()
+    x1 = np.array([0] * 6)
+    x2 = np.linspace(0, 45, 9)
+    x = np.concatenate((x1, x2))
+    y1 = np.linspace(0, -25, 5)
+    y2 = np.linspace(-25, 35, 10)
+    y = np.concatenate((y1, y2))
+    z1 = np.linspace(0, -10, 7)
+    z2 = np.linspace(-10, 25, 8)
+    z = np.concatenate((z1, z2))
+    x = running_mean(x, 4)
+    y = running_mean(y, 4)
+    z = running_mean(z, 4)
+    color = np.abs(x) + 1000 * np.abs(y) + np.abs(z) + 100
+
     page = st.sidebar.selectbox("Choose a page", ["Homepage", "Exploration"])
 
     if page == "Homepage":
         st.header("This is your data explorer.")
         st.write("Please select a page on the left.")
-        st.write(df)
+
     elif page == "Exploration":
         st.title("Data Exploration")
-        x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
-        y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
-        visualize_data(df, x_axis, y_axis)
+        visualize_data(x,y)
 
-@st.cache
-def load_data():
-    df = data.cars()
-    return df
+
+
+def running_mean(l, N):
+    sum = 0
+    result = list(0 for x in l)
+
+    for i in range(0, N):
+        sum = sum + l[i]
+        result[i] = sum / (i + 1)
+
+    for i in range(N, len(l)):
+        sum = sum - l[i - N] + l[i]
+        result[i] = sum / N
+
+    return result
+
 
 def visualize_data(df, x_axis, y_axis):
-    graph = alt.Chart(df).mark_circle(size=60).encode(
-        x=x_axis,
-        y=y_axis,
-        color='Origin',
-        tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
-    ).interactive()
+p = figure(plot_width=400, plot_height=400)
 
-    st.write(graph)
+# add a circle renderer with a size, color, and alpha
+p.circle(x, y, size=20, color="navy", alpha=0.5)
+
+# show the results
+
+    st.write(p)
+
 
 if __name__ == "__main__":
     main()
